69dc17c5e855969e142aba6638a990ec56fd7864
hbin <11323063@qq.com>
2023-12-07 14:30:20 +0800

    导航栏和底部

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,19 @@
+<!-- Footer.vue -->
+<template>
+  <div>
+    <!-- Your Footer content goes here -->
+    <footer>
+      <!-- Footer content -->
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  // Footer component logic
+}
+</script>
+
+<style scoped>
+/* Footer component styles */
+</style>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<!-- Navbar.vue -->
+<template>
+  <div>
+    <!-- Your Navbar content goes here -->
+    <nav>
+      <!-- Navbar content -->
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  // Navbar component logic
+}
+</script>
+
+<style scoped>
+/* Navbar component styles */
+</style>
