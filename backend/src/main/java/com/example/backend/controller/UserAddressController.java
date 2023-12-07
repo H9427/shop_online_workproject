872c483b889fd8328a62b3836278ceeaf61ecbea
wujiahao17 <315776852@qq.com>
2023-12-07 20:57:50 +0800

    最新后端代码

---
+++ backend/src/main/java/com/example/backend/controller/UserAddressController.java
@@ -0,0 +1,13 @@
+package com.example.backend.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/address")
+public class UserAddressController {
+
+
+}
---
+++ backend/src/main/java/com/example/backend/service/impl/UserAddressServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.backend.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.backend.entity.bean.UserAddress;
+import com.example.backend.mapper.UserAddressMapper;
+import com.example.backend.service.UserAddressService;
+
+public class UserAddressServiceImpl extends ServiceImpl<UserAddressMapper, UserAddress> implements UserAddressService {
+}
