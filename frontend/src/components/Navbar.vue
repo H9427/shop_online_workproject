f485099a681551d3c6baa56446dcda6299e9027b
hbin <11323063@qq.com>
2023-12-08 12:24:42 +0800

    主页

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,44 +1,40 @@
 <template>
-  <nav class="bg-base-200">
-    <div role="tablist" class="tabs tabs-bordered">
-      <input style="width: 100px; margin-right: 10px; margin-left: 100px; margin-top: 20px; font-size: 20px" type="radio" name="my_tabs_1" role="tab" class="tab" aria-label="Home" checked />
-      <div role="tabpanel" class="tab-content p-10">Home</div>
-
-      <input style="width: 100px; margin-right: 10px; font-size: 20px" type="radio" name="my_tabs_1" role="tab" class="tab" aria-label="Clothes" />
-      <div role="tabpanel" class="tab-content p-10">Clothes</div>
-
-      <input style="width: 100px; margin-right: 10px; font-size: 20px" type="radio" name="my_tabs_1" role="tab" class="tab" aria-label="Shoes" />
-      <div role="tabpanel" class="tab-content p-10">Shoes</div>
-
-      <!-- 切换风格 -->
-      <div class="flex" style="margin-top: 5px; margin-bottom: 5px; margin-left: 800%">
-        <label class="swap swap-rotate">
-          <input type="checkbox" @click="handleChangeTheme" />
-          <svg class="swap-on fill-current w-7 h-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
-          </svg>
-          <svg class="swap-off fill-current w-7 h-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
-          </svg>
-        </label>
-
-        <button class="btn btn-sm" @click="$router.push('/login')" style="margin-left: 5px; margin-right: 5px">登录</button>
-        <button class="btn btn-sm btn-primary" @click="$router.push('/register')" style="margin-left: 5px; margin-right: 5px">注册</button>
-      </div>
+  <div class="navbar bg-base-200">
+    <div class="navbar-start">
+      <a class="btn btn-ghost text-xl" @click="$router.push('/')">daisyShopping</a>
     </div>
-  </nav>
+    <div class="navbar-center hidden lg:flex">
+      <ul class="menu menu-horizontal px-1">
+        
+        <li><a @click="showClothes">Clothes</a></li>
+        <li><a>Shoes</a></li>
+        <li>
+          <details>
+            <summary>Parent</summary>
+            <ul class="p-2">
+              <li><a>123</a></li>
+              <li><a>456</a></li>
+            </ul>
+          </details>
+        </li>
+      </ul>
+    </div>
+    <div class="navbar-end">
+      <a class="btn" @click="$router.push('/login')">登录/注册</a>
+    </div>
+  </div>
 </template>
 
 <script setup>
-const handleChangeTheme = () => {
-  const html = document.getElementsByTagName("html")[0];
-  const darkTheme = html.dataset.theme;
+const setup = (props, { emit }) => {
+  const showClothes = () => {
+    emit('show-clothes');
+  };
 
-  if (darkTheme === "dark") {
-    html.dataset.theme = "light";
-  } else {
-    html.dataset.theme = "dark";
-  }
+  // 返回供模板使用的数据和方法
+  return {
+    showClothes,
+  };
 };
 </script>
 
