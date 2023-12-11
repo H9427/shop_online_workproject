981e3cab3cb7de347081dbd345c6b4ab29175d09
hbin <11323063@qq.com>
2023-12-11 12:13:21 +0800

    女装页面

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './css/tailwind.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./css/tailwind.css";
+import axios from "axios";
 // import ElementPlus from 'element-plus'
 // import 'element-plus/dist/index.css'
 
-const app = createApp(App)
+axios.defaults.baseURL = "http://172.16.3.64:8080";
+
+const app = createApp(App);
 
 // app.use(ElementPlus)
-app.use(router)
-app.mount('#app')
+app.use(router);
+app.mount("#app");
